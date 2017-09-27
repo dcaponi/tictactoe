42d65c5243f7592dd68aeaca592a0e87a88af7fe
Dominick Caponi <d.caponi1@gmail.com>
2017-09-26 18:31:36 -0700

    make look like cpp style guide

--- board.cpp
+++ board.cpp
@@ -11,106 +11,131 @@
 #include "player.h"
 #include <iostream>
 
-Board::Board(){
+Board::Board()
+{
 	using namespace std;
 	m_size = 0;
 	
-	while ( m_size < 3 ) {
+	while (m_size < 3)
+	{
 		cout << "Enter the size of board. Must be larger than 3: ";
 		cin >> m_size;
 		m_size = cin.fail() ? 0 : m_size;
 		cout << endl;
 	}
 	
-    board = new char*[ m_size ];
-    for( int i = 0; i < m_size; i++ ){
-        board[i] = new char[ m_size ];
-        for( int j = 0; j < m_size; j++ ){
+    board = new char*[m_size];
+    for(int i = 0; i < m_size; i++)
+	{
+        board[i] = new char[m_size];
+        for(int j = 0; j < m_size; j++)
+		{
             board[i][j] = ' ';
         }
     }
 }
 
-char Board::getCharacter( int x, int y ){
+char Board::get_character(int x, int y)
+{
     return board[x][y];
 }
 
-int Board::getSize(){
+int Board::get_size()
+{
     return m_size;
 }
 
-bool Board::update( Player* player ){
-    int* move = player -> getLastMove();
-    board[move[0]][move[1]] = player -> getToken();
+bool Board::update(Player* player)
+{
+    int* move = player -> get_last_move();
+    board[move[0]][move[1]] = player -> get_token();
     delete [] move;
 	m_moves++;
     draw();
-	return computeWinner( player );
+	return compute_winner(player);
 }
 
-bool Board::computeWinner( Player* currentPlayer ){
+bool Board::compute_winner(Player* current_player)
+{
 	using namespace std;
 	
-	bool gameOver = false;
-	int x = currentPlayer -> getLastMove()[0];
-	int y = currentPlayer -> getLastMove()[1];
+	bool game_over = false;
+	int x = current_player -> get_last_move()[0];
+	int y = current_player -> get_last_move()[1];
 	
 	// Check Row
-	for( int i = 0; i < m_size; i++ ){
-		if( board[x][i] != currentPlayer -> getToken() ){
+	for(int i = 0; i < m_size; i++)
+	{
+		if(board[x][i] != current_player -> get_token())
+		{
 			break;
 		}
-		if( i == m_size - 1){
-			cout << currentPlayer -> getName() << " Wins!\n";
-			gameOver = true;
+		if(i == m_size - 1)
+		{
+			cout << current_player -> get_name() << " Wins!\n";
+			game_over = true;
 		}
 	}
 	// Check Col
-	for( int j = 0; j < m_size; j++ ){
-		if( board[j][y] != currentPlayer -> getToken() ){
+	for(int j = 0; j < m_size; j++)
+	{
+		if(board[j][y] != current_player -> get_token())
+		{
 			break;
 		}
-		if( j == m_size - 1){
-			cout << currentPlayer -> getName() << " Wins!\n";
-			gameOver = true;
+		if( j == m_size - 1)
+		{
+			cout << current_player -> get_name() << " Wins!\n";
+			game_over = true;
 		}
 	}
 	
 	// Check /
-	for( int j = m_size - 1; j >= 0; j -- ){
-		if( board[(m_size - 1) - j][j] != currentPlayer -> getToken() ){
+	for(int j = m_size - 1; j >= 0; j --)
+	{
+		if(board[(m_size - 1) - j][j] != current_player -> get_token())
+		{
 			break;
 		}
-		if( j == 0 ){
-			cout << currentPlayer -> getName() << " Wins!\n";
-			gameOver = true;
+		if(j == 0)
+		{
+			cout << current_player -> get_name() << " Wins!\n";
+			game_over = true;
 		}
 	}
 	
 	// Check "\"
-	for( int i = 0; i < m_size; i++ ){
-		if( board[i][i] != currentPlayer -> getToken() ){
+	for(int i = 0; i < m_size; i++)
+	{
+		if(board[i][i] != current_player -> get_token())
+		{
 			break;
 		}
-		if( i == m_size - 1 ){
-			cout << currentPlayer -> getName() << " Wins!\n";
-			gameOver = true;
+		if(i == m_size - 1)
+		{
+			cout << current_player -> get_name() << " Wins!\n";
+			game_over = true;
 		}
 	}
 	
 	// Check Tie
-	if( m_moves == (m_size * m_size - 1) ){
-		gameOver = true;
+	if(m_moves == (m_size * m_size - 1))
+	{
+		game_over = true;
 		cout << "Tie Game!" << endl;
 	}
-	return gameOver;
+	return game_over;
 }
 
-void Board::draw(){
-    for( int i = 0; i < m_size; i++ ){
-        for( int j = 0; j < m_size; j++ ){
+void Board::draw()
+{
+    for(int i = 0; i < m_size; i++)
+	{
+        for(int j = 0; j < m_size; j++)
+		{
             std::cout << " | " << board[i][j];
-            if( j == m_size - 1 ){
+            if(j == m_size - 1)
+			{
                 std::cout << " | " ;
             }
         }
@@ -118,8 +143,10 @@ void Board::draw(){
     }
 }
 
-void Board::destroyBoard(){
-    for( int i = 0; i < m_size; i ++ ){
+void Board::destroy_board()
+{
+    for(int i = 0; i < m_size; i ++)
+	{
         delete [] board[i];
     }
     delete [] board;
--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-void takeTurn( Board* board, Player* player );
+void take_turn(Board* board, Player* player);
 
 int main() {
     
@@ -20,65 +20,70 @@ int main() {
     //cout << "value of first pointer (row array address) " << *board << " " << board[0] << " Same thing" << endl;
     //cout << "value of first entry " << **board << " " << *board[0] << endl;
     
-    bool playAgain = true;
+    bool play_again = true;
 	
-    while( playAgain ){
-        bool gameOver = false;
+    while(play_again){
+        bool game_over = false;
 		
-		Player player1( 'X' );
-		Player player2( 'O' );
-		Player* currentPlayer = &player1;
-		Board gameBoard;
+		Board game_board;
+		Player player1('X');
+		Player player2('O');
+		Player* current_player = &player1;
 		
-		cout << "Player " << player1.getToken() << ": " << player1.getName() << endl;
-		cout << "Player " << player2.getToken() << ": " << player2.getName() << "\n" << endl;
-		
-		gameBoard.draw();
+		cout << "Player " << player1.get_token() << ": " << player1.get_name() << endl;
+		cout << "Player " << player2.get_token() << ": " << player2.get_name() << "\n" << endl;
+		game_board.draw();
 		
 		// Main game loop
-		while( !gameOver ){
-			takeTurn( &gameBoard, currentPlayer );
-			gameOver = gameBoard.update( currentPlayer );
-			currentPlayer = currentPlayer == &player1 ? &player2 : &player1;
+		while(!game_over)
+		{
+			take_turn(&game_board, current_player);
+			game_over = game_board.update(current_player);
+			current_player = current_player == &player1 ? &player2 : &player1;
 		}
 	
-		gameBoard.destroyBoard();
+		game_board.destroy_board();
 		
 		char pa;
 		cout << "Play again? y/n: ";
 		cin >> pa;
-		cout << ( pa == 'y' ) << endl;
-		playAgain = pa == 'y';
+		cout << (pa == 'y') << endl;
+		play_again = pa == 'y';
     }
     return 0;
 }
 
 
 
-void takeTurn( Board* board, Player* player ){
-    bool turnOver = false;
-    int maxEntry = board -> getSize() - 1;
+void take_turn(Board* board, Player* player)
+{
+    bool turn_over = false;
+    int max_entry = board -> get_size() - 1;
     int x, y;
     
-    cout << "Player " << player -> getName() << ", put in (x, y) coords: ";
+    cout << "Player " << player -> get_name() << ", put in (x, y) coords: ";
     cin >> x >> y;
     cout << endl;
 	
 	// Turn loop
-    while( !turnOver ){
-        if( x > maxEntry || y > maxEntry ){
+    while(!turn_over)
+	{
+        if(x > max_entry || y > max_entry)
+		{
             cout << "That choice is out of bounds, please try again: ";
             cin >> x >> y;
             cout << endl;
         }
-        else if( board -> getCharacter( x, y ) != ' ' ){
+        else if(board -> get_character(x, y) != ' ')
+		{
             cout << "That spot is occupied, please try again: ";
             cin >> x >> y;
             cout << endl;
         }
-        else{
-            player -> setMove( x, y );
-            turnOver = true;
+        else
+		{
+            player -> set_move(x, y);
+            turn_over = true;
         }
     }
 }
